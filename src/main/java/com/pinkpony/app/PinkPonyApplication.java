fbe1b760ff5e18f48d2ad3e8b80b9e1e9f3d4800
Pivotal Pair <pair@pivotal.io>
2016-03-17 16:03:24 +0800

    initial commit for pink pony

---
+++ src/main/java/com/pinkpony/app/PinkPonyApplication.java
@@ -0,0 +1,12 @@
+package com.pinkpony.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkPonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkPonyApplication.class, args);
+	}
+}
