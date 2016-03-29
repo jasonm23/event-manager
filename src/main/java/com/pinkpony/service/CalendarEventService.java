a3592a245f23053c908c63cf63918f5d32746402
Stephan February + Jason Milkins <pair+sf+jm@pivotal.io>
2016-03-29 17:19:24 +0800

    Move validators to app config

--- src/main/java/com/pinkpony/service/CalendarEventService.java
+++ src/main/java/com/pinkpony/service/CalendarEventService.java
@@ -1,10 +1,15 @@
 package com.pinkpony.service;
 
 import com.pinkpony.model.CalendarEvent;
+import com.pinkpony.model.CalendarEventMessageProjection;
+import com.pinkpony.model.CalendarEventProjection;
 import com.pinkpony.repository.CalendarEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +17,9 @@ import java.util.Map;
 
 @Service
 public class CalendarEventService {
+
+    @Autowired
+    SpelAwareProxyProjectionFactory spelAwareProxyProjectionFactory;
 
     @Autowired
     CalendarEventRepository calendarEventRepository;
@@ -39,5 +47,26 @@ public class CalendarEventService {
         }
 
         return ResponseEntity.ok(resource);
+    }
+
+    public ResponseEntity<?> createEvent(CalendarEvent calendarEvent, HttpRequest request) {
+
+        //persist our data
+        CalendarEvent savedEvent = calendarEventRepository.save(calendarEvent);
+
+        //construct a project of our data
+        CalendarEventMessageProjection calendarEventProjection = spelAwareProxyProjectionFactory.createProjection(CalendarEventMessageProjection.class, calendarEvent);
+
+        //get the request accept header. inspect
+        if(request.getHeaders().getAccept().contains(MediaType.APPLICATION_JSON)){
+
+        }else if(request.getHeaders().getAccept().contains(MediaType.APPLICATION_JSON)){
+
+        }
+
+        //wrap our projection in a HateOS resource for response
+        Resource<CalendarEventMessageProjection> calendarEventResource = new Resource<>(calendarEventProjection);
+        return ResponseEntity.status(HttpStatus.CREATED).body(calendarEventResource);
+
     }
 }
