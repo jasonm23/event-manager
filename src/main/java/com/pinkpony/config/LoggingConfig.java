eea969fc5674f98185d51344feee5ba5a38bba35
James Rivett-Carnac jrivettcarnac@pivotal.io + Gabe Hollombe <pair+jr+gh@pivotal.io>
2016-03-30 11:28:32 +0800

    Finish configuring marvin
    - post config to marvin on successful builds
    - fail the deploy if post to marvin fails
    - add a logging config class to give simple logging
    [Finishes #116181445]

---
+++ src/main/java/com/pinkpony/config/LoggingConfig.java
@@ -0,0 +1,20 @@
+package com.pinkpony.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class LoggingConfig {
+    // If you'd like to use this, put this in your application.yml:
+    // level.org.springframework.web.filter.CommonsRequestLoggingFilter: DEBUG
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter crlf = new CommonsRequestLoggingFilter();
+        crlf.setIncludeClientInfo(true);
+        crlf.setIncludeQueryString(true);
+        crlf.setIncludePayload(true);
+        return crlf;
+    }
+
+}
