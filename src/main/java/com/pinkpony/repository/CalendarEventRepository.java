19989715b31ebb48e20a3acc6ddde7e88da73017
Hou Yifeng <yhou@pivotal.io>
2016-03-30 12:04:47 +0800

    get upcoming events by custom query method in repository
    [#115836895]

--- src/main/java/com/pinkpony/repository/CalendarEventRepository.java
+++ src/main/java/com/pinkpony/repository/CalendarEventRepository.java
@@ -2,9 +2,19 @@ package com.pinkpony.repository;
 
 import com.pinkpony.model.CalendarEvent;
 import com.pinkpony.model.CalendarEventProjection;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Date;
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = CalendarEventProjection.class)
 public interface CalendarEventRepository extends PagingAndSortingRepository<CalendarEvent, Long> {
+    List<CalendarEvent> findByCalendarEventDateTimeAfter(Date date);
+
+    @RestResource(path = "upcomingEvents")
+    @Query(value = "SELECT * FROM calendar_event WHERE calendar_event_date_time >= NOW() AND cancelled = 'f' ORDER BY calendar_event_date_time ASC;", nativeQuery = true)
+    List<CalendarEvent> findUpcomingEvent();
 }
---
+++ src/test/java/com/pinkpony/integration/UpcomingCalendarEventTest.java
@@ -0,0 +1,100 @@
+package com.pinkpony.integration;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.pinkpony.PinkPonyApplication;
+import com.pinkpony.model.CalendarEvent;
+import com.pinkpony.repository.CalendarEventRepository;
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Date;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PinkPonyApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class UpcomingCalendarEventTest {
+
+    @Autowired
+    CalendarEventRepository calendarEventRepository;
+
+    @Value("${local.server.port}")
+    int port;
+
+    private DateTime today = new DateTime();
+    private DateTime yesterday = today.minusDays(1);
+    private DateTime tomorrow = today.plusDays(1);
+    private DateTime nextWeek = today.plusWeeks(1);
+
+    private String upcomingUrl = "/calendarEvents/search/upcomingEvents";
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    @After
+    public void tearDown() {
+        calendarEventRepository.deleteAll();
+    }
+
+    private CalendarEvent makeCalendarEvent(Date date) {
+        CalendarEvent newCalendarEvent = new CalendarEvent();
+        newCalendarEvent.setName("Spring Boot Night");
+        newCalendarEvent.setDescription("Wanna learn how to boot?");
+        newCalendarEvent.setVenue("Arrowhead Lounge");
+        newCalendarEvent.setCalendarEventDateTime(date);
+        newCalendarEvent.setUsername("Holly");
+
+        return newCalendarEvent;
+    }
+
+    /*
+     * An upcoming event is one that:
+     * - is in the future AND
+     * - is not cancelled
+     *
+     * Upcoming events must be returned soonest first.
+     */
+    @Test
+    public void getUpcomingEvents() {
+        CalendarEvent pastEvent = makeCalendarEvent(yesterday.toDate());
+        pastEvent.setName("past event");
+        CalendarEvent futureEventTmr = makeCalendarEvent(tomorrow.toDate());
+        futureEventTmr.setName("future event");
+        CalendarEvent futureCancelledEvent = makeCalendarEvent(tomorrow.toDate());
+        futureCancelledEvent.setName("future cancelled event");
+        futureCancelledEvent.setCancelled(true);
+        CalendarEvent futureEventNextWeek = makeCalendarEvent(nextWeek.toDate());
+        futureEventNextWeek.setName("future event next week");
+
+        calendarEventRepository.save(pastEvent);
+        calendarEventRepository.save(futureEventNextWeek);
+        calendarEventRepository.save(futureEventTmr);
+        calendarEventRepository.save(futureCancelledEvent);
+
+        given().
+            contentType(ContentType.JSON).
+        when().
+            get(upcomingUrl).
+        then().
+            statusCode(200).
+            body("_embedded.calendarEvents", hasSize(2)).
+            body("_embedded.calendarEvents[0].name", equalTo("future event")).
+            body("_embedded.calendarEvents[1].name", equalTo("future event next week"));
+    }
+}
