bf9818108115682a9c7b36988a655910c4423840
James Rivett-Carnac jrivettcarnac@pivotal.io + Yifeng Hou yhou@pivotal.io <pair+jr+yh@pivotal.io>
2016-03-31 11:08:31 +0800

    Add calendarEventDateTime into default projections
    [Finishes #115836895]

--- src/main/java/com/pinkpony/model/CalendarEventProjection.java
+++ src/main/java/com/pinkpony/model/CalendarEventProjection.java
@@ -1,5 +1,6 @@
 package com.pinkpony.model;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.util.List;
@@ -10,6 +11,9 @@ public interface CalendarEventProjection {
     String getDescription();
     String getUsername();
     String getVenue();
+
+    @Value("#{ target.getFormattedEventDateTime() }")
+    String getCalendarEventDateTime();
     Boolean getCancelled();
 
     List<Rsvp> getRsvps();
