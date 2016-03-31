e46d600accb37d815cf78f2a7dc0076ed401454e
Stephan February + Jason Milkins <pair+sf+jm@pivotal.io>
2016-03-31 17:19:31 +0800

    Cleanup AppConfig

--- src/main/java/com/pinkpony/config/AppConfig.java
+++ src/main/java/com/pinkpony/config/AppConfig.java
@@ -1,14 +1,10 @@
 package com.pinkpony.config;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.pinkpony.validator.CalendarEventValidator;
-import com.pinkpony.validator.RsvpValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.http.MediaType;
 import org.springframework.validation.DefaultMessageCodesResolver;
-import org.springframework.validation.MessageCodeFormatter;
 import org.springframework.validation.MessageCodesResolver;
 
 @Configuration
@@ -23,16 +19,6 @@ public class AppConfig {
         resolver.setMessageCodeFormatter(DefaultMessageCodesResolver.Format.PREFIX_ERROR_CODE);
 
         return resolver;
-    }
-
-    @Bean
-    public RsvpValidator rsvpValidator(){
-        return new RsvpValidator();
-    }
-
-    @Bean
-    public CalendarEventValidator calendarEventValidator(){
-        return new CalendarEventValidator();
     }
 
     @Bean
