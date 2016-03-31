a01db80cd4d854628b13a7e45d804a9a8f647ad0
Stephan February + Jason Milkins <pair+sf+jm@pivotal.io>
2016-03-31 17:59:08 +0800

    [no-pt-story] Hotfix to repair REST behavior of other HTTP Methods

---
+++ src/main/java/com/pinkpony/controller/CalendarEventRepositoryRestController.java
@@ -0,0 +1,34 @@
+package com.pinkpony.controller;
+
+import com.pinkpony.config.MarvinMediaTypes;
+import com.pinkpony.model.CalendarEvent;
+import com.pinkpony.repository.CalendarEventRepository;
+import com.pinkpony.service.CalendarEventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RepositoryRestController
+public class CalendarEventRepositoryRestController {
+
+    @Autowired
+    CalendarEventService calendarEventService;
+
+    @Autowired
+    MessageCodesResolver messageCodesResolver;
+
+    @Autowired
+    CalendarEventRepository calendarEventRepository;
+
+    @RequestMapping(value="/calendarEvents", method = RequestMethod.POST, consumes = {MediaType.ALL_VALUE, MediaType.APPLICATION_JSON_VALUE, MarvinMediaTypes.MARVIN_JSON_MEDIATYPE_VALUE})
+    public @ResponseBody ResponseEntity<ResourceSupport> createEvent(@RequestBody CalendarEvent calendarEvent, HttpServletRequest request) {
+        return calendarEventService.createEvent(calendarEvent, request);
+    }
+}
