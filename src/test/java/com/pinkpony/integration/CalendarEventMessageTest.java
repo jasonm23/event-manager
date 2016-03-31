991e47e657eec4dd1febc5499ea5618c36443a7a
Stephan February + Jason Milkins <pair+sf+jm@pivotal.io>
2016-03-31 15:39:25 +0800

    Add test for event message projection

--- src/test/java/com/pinkpony/integration/CalendarEventMessageTest.java
+++ src/test/java/com/pinkpony/integration/CalendarEventMessageTest.java
@@ -5,7 +5,23 @@ import org.junit.Test;
 public class CalendarEventMessageTest extends PinkPonyIntegrationBase {
 
     @Test
-    public void calendarEventMessageProjection() {
+    public void calendarEventMessageProjection() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("name","Bob's Blowout");
+        json.put("description","Big party for Uncle Bob");
+        json.put("username","Joe Smoove");
+        json.put("calendarEventDateTime", calendarEventDateString);
+        json.put("venue","Da Hacienda");
 
+        given().
+                accept(AppConfig.MARVIN_JSON_MEDIATYPE_VALUE).
+                contentType(ContentType.JSON).
+                body(json.toString()).
+        when().
+                post("/calendarEvents?projection=eventMessage").
+        then().log().all().
+                statusCode(201).
+                body("message", equalTo("event Bob's Blowout created")).
+                body("message_type", equalTo("channel"));
     }
 }
