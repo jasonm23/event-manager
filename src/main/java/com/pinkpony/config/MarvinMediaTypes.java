1ece1a9341d8b2a43af651f0d8cc2d4004d0151c
Stephan February + James Rivett-Carnac <pair+sf+jr@pivotal.io>
2016-04-06 10:07:15 +0800

    Change the media type to vnd.marvin-bot for messages
    [#116277605]

--- src/main/java/com/pinkpony/config/MarvinMediaTypes.java
+++ src/main/java/com/pinkpony/config/MarvinMediaTypes.java
@@ -3,6 +3,6 @@ package com.pinkpony.config;
 import org.springframework.http.MediaType;
 
 public class MarvinMediaTypes {
-    public static final MediaType MARVIN_JSON_MEDIATYPE = new MediaType("application", "x-marvin-bot+json");
-    public static final String MARVIN_JSON_MEDIATYPE_VALUE = "application/x-marvin-bot+json";
+    public static final MediaType MARVIN_JSON_MEDIATYPE = new MediaType("application", "vnd.marvin-bot+json");
+    public static final String MARVIN_JSON_MEDIATYPE_VALUE = "application/vnd.marvin-bot+json";
 }
